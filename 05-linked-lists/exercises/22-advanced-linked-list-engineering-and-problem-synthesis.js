7ba39ef2f325085223db4b3a6f206faadfaebcba
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:21:15 +0530

    Add Phase 05.22 advanced linked-list exercises

---
+++ 05-linked-lists/exercises/22-advanced-linked-list-engineering-and-problem-synthesis.js
@@ -0,0 +1,23 @@
+// Phase 05.22 — Advanced Linked List Engineering & Problem Synthesis
+// Intentionally UNSOLVED.
+
+function selectLinkedStructure(workload) { // TODO }
+function defineListInvariants(list) { // TODO }
+function applyPointerMutation(list, operation) { // TODO }
+function validateMutationTransaction(before, after) { // TODO }
+function createSentinelList() { // TODO }
+function validateNodeOwnership(list, node) { // TODO }
+function analyzeAliasing(roots) { // TODO }
+function classifyCycleStructure(head) { // TODO }
+function compareDestructiveAndPersistent(problem) { // TODO }
+function analyzeStructuralSharing(oldRoot, newRoot) { // TODO }
+function analyzeSkipListWorkload(problem) { // TODO }
+function analyzeIntrusiveListMembership(object, lists) { // TODO }
+function analyzeLinkedStructureMemory(roots) { // TODO }
+function analyzeConcurrentMutation(history) { // TODO }
+function detectABA(history) { // TODO }
+function designCancellableQueue(problem) { // TODO }
+function designAISearchArchitecture(problem) { // TODO }
+function generateOperationSequence(seed, count) { // TODO }
+function compareWithReferenceModel(structure, operations) { // TODO }
+function linkedListEngineeringSynthesis(problem) { // TODO }
