130fbfc711113e47c0d0eb2b89e7ee82cd71f87a
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:23:08 +0530

    Add Phase 05.23 linked-list mastery exercises

---
+++ 05-linked-lists/exercises/23-linked-list-mastery-advanced-problem-patterns.js
@@ -0,0 +1,23 @@
+// Phase 05.23 — Linked List Mastery: Advanced Problem Patterns & Interview Preparation
+// Intentionally UNSOLVED.
+
+function findMiddleWithFastSlow(head) { // TODO }
+function findKthFromEndWithGap(head, k) { // TODO }
+function removeNthFromEndWithSentinel(head, n) { // TODO }
+function reverseListWithInvariant(head) { // TODO }
+function reverseSegment(head, left, right) { // TODO }
+function reverseGroups(head, k, incompletePolicy) { // TODO }
+function partitionStable(head, pivot) { // TODO }
+function mergeSortedListsStable(headA, headB, compare) { // TODO }
+function mergeKSortedLists(lists, compare, strategy) { // TODO }
+function findIntersectionByIdentity(headA, headB) { // TODO }
+function findCycleEntry(head) { // TODO }
+function isPalindromeAndRestore(head) { // TODO }
+function removeDuplicatesPreserveIdentity(head) { // TODO }
+function createMapLinkedListIndex(items) { // TODO }
+function cancelIndexedNode(index, id) { // TODO }
+function persistentListUpdate(head, index, value) { // TODO }
+function validateLinkedListInvariants(list) { // TODO }
+function generateLinkedListEdgeCases(values) { // TODO }
+function compareAgainstReferenceModel(structure, operations) { // TODO }
+function linkedListInterviewSynthesis(problem) { // TODO }
