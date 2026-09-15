7ee9c8eec792774e3267b2795c68201f10b569fe
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:10:53 +0530

    Add Phase 05.14 skip list exercises

---
+++ 05-linked-lists/exercises/14-skip-lists-probabilistic-structures.js
@@ -0,0 +1,23 @@
+// Phase 05.14 — Skip Lists & Probabilistic Linked Structures
+// Intentionally UNSOLVED.
+
+function createSkipNode(value, level) { // TODO }
+function createSkipList(maxLevel, probability) { // TODO }
+function randomLevel(random, maxLevel, probability) { // TODO }
+function skipListSearch(list, target, compare) { // TODO }
+function skipListLowerBound(list, target, compare) { // TODO }
+function skipListUpperBound(list, target, compare) { // TODO }
+function findUpdatePredecessors(list, target, compare) { // TODO }
+function skipListInsert(list, value, compare, random) { // TODO }
+function skipListDelete(list, target, compare) { // TODO }
+function skipListRangeQuery(list, start, end, compare) { // TODO }
+function skipListFindFirstDuplicate(list, target, compare) { // TODO }
+function skipListFindLastDuplicate(list, target, compare) { // TODO }
+function validateSkipListLevels(list, compare) { // TODO }
+function validateLevelZeroCompleteness(list) { // TODO }
+function analyzeSkipListHeight(list) { // TODO }
+function estimateExpectedPointerCount(nodeCount, probability) { // TODO }
+function compareSkipListAndBalancedTree(problem) { // TODO }
+function compareSkipListAndHashTable(problem) { // TODO }
+function testSkipListWithDeterministicRandomness(list, operations) { // TODO }
+function skipListEngineeringSynthesis(problem) { // TODO }
