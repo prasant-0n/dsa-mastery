b772985965bffdc2a3516ed732ec23868049870e
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:06:15 +0530

    Add Phase 05.11 exercises

---
+++ 05-linked-lists/exercises/11-stacks-queues-and-deques.js
@@ -0,0 +1,23 @@
+// Phase 05.11 — Linked Lists as Stacks, Queues & Deques
+// Intentionally UNSOLVED.
+
+function stackPush(stack, value) { // TODO }
+function stackPop(stack) { // TODO }
+function stackPeek(stack) { // TODO }
+function queueEnqueue(queue, value) { // TODO }
+function queueDequeue(queue) { // TODO }
+function queuePeek(queue) { // TODO }
+function dequePushFront(deque, value) { // TODO }
+function dequePushBack(deque, value) { // TODO }
+function dequePopFront(deque) { // TODO }
+function dequePopBack(deque) { // TODO }
+function dequePeekFront(deque) { // TODO }
+function dequePeekBack(deque) { // TODO }
+function handleQueueUnderflow(queue) { // TODO }
+function handleQueueCapacity(queue, value) { // TODO }
+function validateStackInvariants(stack) { // TODO }
+function validateQueueInvariants(queue) { // TODO }
+function validateDequeInvariants(deque) { // TODO }
+function analyzeQueueBackpressure(problem) { // TODO }
+function compareLinkedQueueAndRingBuffer(problem) { // TODO }
+function linkedListAdtSynthesis(problem) { // TODO }
