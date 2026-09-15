897f2ae1464d5bce20b66864ee67c547b0e007d6
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:12:20 +0530

    Add Phase 05.15 intrusive linked list exercises

---
+++ 05-linked-lists/exercises/15-intrusive-linked-lists-zero-allocation-design.js
@@ -0,0 +1,23 @@
+// Phase 05.15 — Intrusive Linked Lists & Zero-Allocation Data-Structure Design
+// Intentionally UNSOLVED.
+
+function createIntrusiveNode(payload) { // TODO }
+function insertIntrusiveFront(list, node) { // TODO }
+function insertIntrusiveBack(list, node) { // TODO }
+function removeIntrusiveNode(list, node) { // TODO }
+function detachIntrusiveNode(list, node) { // TODO }
+function moveIntrusiveNodeToFront(list, node) { // TODO }
+function moveIntrusiveNodeToBack(list, node) { // TODO }
+function validateIntrusiveMembership(list, node) { // TODO }
+function validateIntrusiveInvariants(list) { // TODO }
+function detectDoubleInsertion(list, node) { // TODO }
+function createSentinelIntrusiveList() { // TODO }
+function insertSentinelIntrusiveNode(list, node) { // TODO }
+function removeSentinelIntrusiveNode(list, node) { // TODO }
+function createMultiMembershipObject(payload) { // TODO }
+function moveBetweenIntrusiveLists(source, destination, node) { // TODO }
+function estimateIntrusiveAllocationSavings(itemCount) { // TODO }
+function compareIntrusiveAndWrapperLists(problem) { // TODO }
+function designIntrusiveLRU(problem) { // TODO }
+function analyzeZeroAllocationOperation(operation) { // TODO }
+function intrusiveListEngineeringSynthesis(problem) { // TODO }
