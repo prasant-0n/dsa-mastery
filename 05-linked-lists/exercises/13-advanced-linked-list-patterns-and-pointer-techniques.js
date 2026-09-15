3445764bb775e75ccb2b5e86ea6dfd6dbf5bcb50
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:08:36 +0530

    Add Phase 05.13 advanced linked list pointer exercises

---
+++ 05-linked-lists/exercises/13-advanced-linked-list-patterns-and-pointer-techniques.js
@@ -0,0 +1,23 @@
+// Phase 05.13 — Advanced Linked List Patterns & Pointer Techniques
+// Intentionally UNSOLVED.
+
+function removeNthFromEnd(head, n) { // TODO }
+function findKthFromEndOnePass(head, k) { // TODO }
+function swapPairs(head) { // TODO }
+function reverseSegment(head, left, right) { // TODO }
+function reverseInGroups(head, k) { // TODO }
+function partitionStable(head, pivot) { // TODO }
+function partitionThreeWay(head, pivot) { // TODO }
+function isPalindromeRestoreList(head) { // TODO }
+function findIntersectionByPointerSwitching(headA, headB) { // TODO }
+function findCycleEntryFloyd(head) { // TODO }
+function decomposeCycle(head) { // TODO }
+function mergeTwoSortedListsStable(headA, headB, compare) { // TODO }
+function mergeKSortedListsDivideAndConquer(lists, compare) { // TODO }
+function mergeKSortedListsWithHeap(lists, compare) { // TODO }
+function validateStableMerge(original, merged, keySelector) { // TODO }
+function analyzeNodeIdentityAfterTransformation(original, transformed) { // TODO }
+function persistentPrepend(list, value) { // TODO }
+function createListIterator(head) { // TODO }
+function validatePointerTransformation(before, after) { // TODO }
+function advancedLinkedListSynthesis(problem) { // TODO }
