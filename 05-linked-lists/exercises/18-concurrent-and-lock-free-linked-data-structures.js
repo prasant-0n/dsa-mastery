e1e0ed7332c9d82ca242efc9d256979fb5755aa9
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:16:45 +0530

    Add Phase 05.18 concurrency and lock-free linked structure exercises

---
+++ 05-linked-lists/exercises/18-concurrent-and-lock-free-linked-data-structures.js
@@ -0,0 +1,23 @@
+// Phase 05.18 — Concurrent & Lock-Free Linked Data Structures
+// Intentionally UNSOLVED.
+
+function modelConcurrentRemoval(state) { // TODO }
+function identifyCriticalSection(operation) { // TODO }
+function analyzeLockOrdering(locks) { // TODO }
+function detectPotentialDeadlock(waitGraph) { // TODO }
+function classifyProgressGuarantee(algorithm) { // TODO }
+function simulateCompareAndSwap(state, expected, desired) { // TODO }
+function designLockFreeStack(initialValues) { // TODO }
+function simulateLockFreePush(state, node) { // TODO }
+function simulateLockFreePop(state) { // TODO }
+function detectABASequence(history) { // TODO }
+function createTaggedReference(pointer, version) { // TODO }
+function compareTaggedReferences(a, b) { // TODO }
+function modelHazardPointerProtection(threads, nodes) { // TODO }
+function analyzeEpochReclamation(epochs, retiredNodes) { // TODO }
+function validateConcurrentListInvariants(list) { // TODO }
+function classifyJavaScriptConcurrencyModel(problem) { // TODO }
+function compareSharedMemoryAndMessagePassing(problem) { // TODO }
+function linearizabilityCheck(history) { // TODO }
+function stressConcurrentQueue(problem) { // TODO }
+function concurrentLinkedStructureSynthesis(problem) { // TODO }
