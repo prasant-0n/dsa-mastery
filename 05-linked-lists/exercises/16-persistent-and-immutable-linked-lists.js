b54c20b694f9bd25a2c75719b839711fd213044c
Prasanta Kumar Sahoo <92263503+prasant-0n@users.noreply.github.com>
2026-09-16 00:14:34 +0530

    Add Phase 05.16 persistent linked list exercises

---
+++ 05-linked-lists/exercises/16-persistent-and-immutable-linked-lists.js
@@ -0,0 +1,23 @@
+// Phase 05.16 — Persistent & Immutable Linked Lists
+// Intentionally UNSOLVED.
+
+function createImmutableNode(value, next) { // TODO }
+function persistentPrepend(head, value) { // TODO }
+function persistentPop(head) { // TODO }
+function persistentGet(head, index) { // TODO }
+function persistentUpdate(head, index, value) { // TODO }
+function persistentAppend(head, value) { // TODO }
+function persistentInsertAt(head, index, value) { // TODO }
+function persistentDeleteAt(head, index) { // TODO }
+function collectSharedNodes(headA, headB) { // TODO }
+function compareStructuralSharing(oldHead, newHead) { // TODO }
+function validateImmutableVersion(oldHead, newHead) { // TODO }
+function buildPersistentStack(operations) { // TODO }
+function analyzeVersionGraph(roots) { // TODO }
+function estimatePersistentAllocation(head, operation) { // TODO }
+function comparePersistentAndMutableUpdate(problem) { // TODO }
+function comparePersistentAndFullCopy(problem) { // TODO }
+function analyzeRetainedMemory(roots) { // TODO }
+function createPersistentQueue(front, back) { // TODO }
+function persistentQueueEnqueue(queue, value) { // TODO }
+function persistentLinkedListSynthesis(problem) { // TODO }
